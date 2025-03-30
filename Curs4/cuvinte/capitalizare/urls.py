955a60e4e4f56f35710671db5c126b9c3cbaaa78
CristianMuscalu1979 <muscaluemilcristian@gmail.com>
2025-03-30 12:15:46 +0300

    Capitalizare

---
+++ Curs4/cuvinte/capitalizare/urls.py
@@ -0,0 +1,9 @@
+###  URL MIC
+from django.urls import path
+from .views import capitalizare_view,parametrii_view
+
+urlpatterns = [
+    path('parametrii',parametrii_view),
+    path('<text>',capitalizare_view),
+    
+]
---
+++ Curs4/cuvinte/capitalizare/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def capitalizare_view(request,text):
+    return HttpResponse(text.upper())
+
+def parametrii_view(request):
+    print(request.GET)
+    primit= request.GET.get('text')
+    print("Ai primit", primit)
+    if primit:
+        return HttpResponse(primit.upper())
+    else:
+        return HttpResponse("Nu am gasit un parametru text...")
