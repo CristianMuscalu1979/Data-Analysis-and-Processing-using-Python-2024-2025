2c487026916a1df1acf1f6353f249197ad1c4fc6
CristianMuscalu1979 <muscaluemilcristian@gmail.com>
2025-04-06 16:28:31 +0300

    Pomul de Paste

---
+++ Curs5/milan/formatie/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+import pycountry
+
+
+def formatie_2007_view(request):
+    ##https://dontpad.com/2007
+
+  
+    
+    formatie = [ ('Dida', 'BR'), 
+                ('Oddo', 'IT'),
+                ('Nesta', 'IT'),  
+                ('Maldini', 'IT'),
+                ('Jankulovski', 'CZ') ,
+                ('Gattuso', 'IT'),
+                ('Pirlo', 'IT'), 
+                ('Ambrosini', 'IT') , 
+                ('Kaka', 'BR'),
+                ('Seedors', 'NL'),
+                ('Inzaghi', 'IT')]
+
+
+    formatie = [ (j, pycountry.countries.get(alpha_2=n).flag )  for j,n in formatie  ]
+
+    pozitionare = [1, 2, 3, 4, 1]
+
+
+    pozitionare_formatie = []
+    counter = 0
+    for i in pozitionare:
+        pozitionare_formatie.append(formatie[counter: counter + i])
+        counter += i
+        
+
+
+    context = {
+        # 'linii': [range(1), range(3), range(4), range(2), range(1)]
+        'linii': pozitionare_formatie
+    }
+    return render(request, "2007.html", context)
+
+def formatie_1994_view(request):
+    ##https://dontpad.com/2007
+    return render(request, "fotbal.html")
